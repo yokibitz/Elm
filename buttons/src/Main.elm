cb430299417ff6bd81f20159b8049b0633195f61
yokibitz <yokibitz@hotmail.com>
2017-10-12 05:41:01 +0800

    commit

---
+++ buttons/src/Main.elm
@@ -0,0 +1,39 @@
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+main = 
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+type alias Model = 
+  Int
+
+model : Model
+model = 
+  0
+
+type Msg = Increment | Decrement | Reset 
+
+view : Model -> Html Msg
+view model =
+  div []
+  [
+    div []
+    [
+      button [onClick Reset] [text "Reset"]
+    ]    
+    , button [onClick Decrement] [text "-"]
+    , div [] [text (toString model)]
+    , button [onClick Increment] [text "+"]
+  ]
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+    Decrement ->
+      model - 1
+    Reset ->
+      model - model
+
+            
