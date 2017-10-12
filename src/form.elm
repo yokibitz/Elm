263c6f9cd546d507c8de8f48501fdfe1919e8533
yokibitz <yokibitz@hotmail.com>
2017-10-13 07:06:26 +0800

    added gitignore, form without button validation

---
+++ src/form.elm
@@ -0,0 +1,79 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+import Char exposing (isUpper, isLower, isDigit)
+
+main = 
+  Html.beginnerProgram { model = model, view = view, update = update}
+
+type alias Model = 
+  {
+    name : String,
+    password : String,
+    passwordAgain : String,
+    age : String
+  }
+
+model : Model
+model = 
+  Model "" "" "" ""
+
+type Msg =
+  Name String
+  | Password String
+  | PasswordAgain String
+  | Age String
+  --| Submit
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+      Name newName ->
+        {model | name = newName}
+      Password newPassword ->
+        {model | password = newPassword}
+      PasswordAgain newPasswordAgain ->
+        {model | passwordAgain = newPasswordAgain}      
+      Age newAge ->
+        {model | age = newAge}
+      -- Submit ->
+      --   viewValidation model
+      --   model
+
+
+view : Model -> Html Msg
+view model =
+  div[]
+  [
+    input [type_ "text", placeholder "name", onInput Name ] []
+    , input [type_ "text", placeholder "age", onInput Age ] []
+    , input [type_ "password", placeholder "password", onInput Password ] []
+    , input [type_ "password", placeholder "password again", onInput PasswordAgain ] []    
+    , viewValidation model
+    --, div [] [button [onClick Submit] [text "Submit"]]
+  ]
+
+viewValidation : Model -> Html msg
+viewValidation model =
+  let
+    (color, message) = 
+      if (not <| String.all isDigit model.age) then
+        ("red", "age must be a number")
+      else if(model.password /= model.passwordAgain) then
+        ("red", "passwords don't match")
+      else if (String.length model.password < 8) then
+        ("red", "password must be at least 8 characters")
+      else if (not (String.any isLower model.password && String.any isUpper model.password && String.any isDigit model.password)) then
+        ("red", "password must be contain upper case, lower case, and numeric characters")
+      else        
+        ("green","OK")
+  in
+    div [style [("color", color)]] [text message]
+
+
+
+    
+
+
+
