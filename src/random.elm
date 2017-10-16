78cc780d49e30656ce8f685210a60d7f4b763278
yokibitz <yokibitz@hotmail.com>
2017-10-17 06:30:25 +0800

    two dice, image face

--- src/random.elm
+++ src/random.elm
@@ -1,10 +1,10 @@
 module Main exposing (..)
 
-import Html exposing (Html, button, div, h1, text, img)
+import Dict exposing (Dict)
+import Html exposing (Html, button, div, h1, img, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Random
-import Dict exposing (Dict)
 
 
 main =
@@ -17,43 +17,55 @@ main =
 
 
 type alias Model =
-    { dieFace : Int
+    { dieFace1 : Int
+    , dieFace2 : Int
     }
 
 
 type Msg
     = Roll
-    | NewFace Int
+    | NewFace (Int, Int)
+
 
 imageDict : Dict Int String
-imageDict = 
-  Dict.fromList 
-    [
-      (1, "one.png")
-      , (2, "two.png")
-      , (3, "three.png")
-      , (4, "four.png")
-      , (5, "five.png")
-      , (6, "six.png")
-    ]
+imageDict =
+    Dict.fromList
+        [ ( 1, "one.png" )
+        , ( 2, "two.png" )
+        , ( 3, "three.png" )
+        , ( 4, "four.png" )
+        , ( 5, "five.png" )
+        , ( 6, "six.png" )
+        ]
 
+dieGenerator : Random.Generator Int
+dieGenerator =
+    Random.int 1 6
+
+diePairGenerator : Random.Generator (Int,Int)
+diePairGenerator = 
+    Random.pair dieGenerator dieGenerator
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Roll ->
-            ( model, Random.generate NewFace (Random.int 1 6) )
+            ( model, Random.generate NewFace diePairGenerator)
 
-        NewFace newFace ->
-            ( Model newFace , Cmd.none )
+        NewFace (newFace1, newFace2) ->
+            ( Model newFace1 newFace2, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text (toString model.dieFace) ]
-        -- , img [src ] 
-        , button [ onClick Roll ] [ text "Roll" ]
+        [ h1 [] [ text (toString model.dieFace1) ]
+        , img [ src <| "..\\images\\" ++ (Maybe.withDefault "" <| Dict.get model.dieFace1 imageDict) ] []
+        , h1 [] [ text (toString model.dieFace2) ]
+        , img [ src <| "..\\images\\" ++ (Maybe.withDefault "" <| Dict.get model.dieFace2 imageDict) ] []
+        , div []
+            [ button [ onClick Roll ] [ text "Roll" ]
+            ]
         ]
 
 
@@ -64,4 +76,4 @@ subscriptions model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model 1, Cmd.none )
+    ( Model 1 1, Cmd.none )
