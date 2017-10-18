f70db67b28c72a7b66275e105f13f4adc725a631
yokibitz <yokibitz@hotmail.com>
2017-10-19 06:57:40 +0800

    http_giphy : done with additional exercise scenarios

--- src/http_giffy.elm
+++ src/http_giffy.elm
@@ -1,71 +1,84 @@
-import Html exposing (Html, div, text, button, h2, img)
+import Html exposing (Html, Attribute, div, text, button, h2, img, br, input, select, option)
 import Http
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, on, targetValue)
 import Json.Decode as Decode
   
-main : Program Never Model Msg
-main =
-    Html.program
-    { init = init
-      , view = view
-      , update = update
-      , subscriptions = subscriptions
-    }
-  
-  
-type alias Model =
-  { 
-    topic : String
-    , gifUrl : String
+main = 
+  Html.program
+  {
+    init = init "cats"
+    , view = view
+    , update = update
+    , subscriptions = subscriptions         
   }
 
-init : (Model, Cmd Msg)
-init = 
-  (Model "cats" "waiting.gif", Cmd.none)
-
+type alias Model =
+  {
+    topic : String,
+    gifUrl : String,
+    errorMessage : String
+  }
 
 type Msg = 
   MorePlease
+  | NewTopic String
   | NewGif (Result Http.Error String)
 
-
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-  case msg of
-      MorePlease ->
-          (model, getRandomGif model.topic)
-      NewGif (Ok newUrl) ->
-          ({model | gifUrl = newUrl}, Cmd.none)          
-      NewGif (Err _) ->
-          (model, Cmd.none)
-      
-getRandomGif : String -> Cmd Msg
-getRandomGif topic = 
-  let
-      url = 
-        "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
-      request = 
-        Http.get url decodeGifUrl
-  in
-      Http.send NewGif request
-
-decodeGifUrl : Decode.Decoder String
-decodeGifUrl = 
-  Decode.at ["data", "image_url"] Decode.string
-
+init : String -> (Model, Cmd Msg)
+init topic =
+  (Model topic "waiting.gif" "", getRandomGif topic)
 
 view : Model -> Html Msg
 view model =
-  div []
-      [ 
-        h2 [] [text model.topic]
-        , img [src model.gifUrl] []
-        , button [onClick MorePlease] [text "More Please!"]
+    div []
+    [
+      h2 [] [text model.topic]
+      , select [onChange NewTopic, selected True] 
+      [
+        option [] [text "cats"] 
+        , option [] [text "dogs"] 
+        , option [] [text "cakes"] 
       ]
+      , button [onClick MorePlease] [text "More please!!!"]
+      , br [] []
+      , img [src model.gifUrl] []
+      , div [style [("color", "red")]] [text model.errorMessage]
+    ]
 
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        MorePlease ->
+            ( {model | errorMessage = ""}, getRandomGif model.topic )
+
+        NewTopic newTopic ->
+            ( {model | topic = newTopic}, Cmd.none )    
+        
+        NewGif (Ok value) ->
+            ( {model | gifUrl = value, errorMessage = ""}, Cmd.none )
+            
+        NewGif (Err err) ->
+            ( {model | errorMessage = toString err} , Cmd.none )
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+  let
+    url = 
+      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+    request = 
+      Http.get url decodeGifUrl
+  in
+    Http.send NewGif request
+
+decodeGifUrl : Decode.Decoder String
+decodeGifUrl =
+  Decode.at["data", "image_url"] Decode.string  
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
 
+onChange : (String -> msg) -> Attribute msg
+onChange tagger =
+    on "change" (Decode.map tagger targetValue)
